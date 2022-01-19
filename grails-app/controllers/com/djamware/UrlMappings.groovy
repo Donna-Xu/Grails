1da72bdfac6e9b6661b7ea4d9a5f61b53e1aaf83
xud1atwit <60639756+Donna-Xu@users.noreply.github.com>
2022-01-19 16:48:52 -0500

    Create UrlMappings.groovy

---
+++ grails-app/controllers/com/djamware/UrlMappings.groovy
@@ -0,0 +1,28 @@
+  } catch (ValidationException e) {
+            respond product.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'product.label', default: 'Pro$                redirect product
+            }
+            '*'{ respond product, [status: OK] }
+        }
+    }
+    @Secured('ROLE_ADMIN')
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        productService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'product.label', default: 'Pro$                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
