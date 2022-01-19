28cb58bceeb1826964db8303c68c7c34af64ad09
xud1atwit <60639756+Donna-Xu@users.noreply.github.com>
2022-01-19 16:43:44 -0500

    Create resources.groovy

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,8 @@
+
+import com.djamware.UserPasswordEncoderListener
+import com.djamware.CustomUserDetailsService
+// Place your Spring DSL code here
+beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
+}
